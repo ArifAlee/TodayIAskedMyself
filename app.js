9ead2f63b9dfa77507c78e985130e1d7a5fc1c61
Arif Ali <Arifalee241@gmail.com>
2025-07-02 22:52:31 +0100

    FIXED ALIGNMENT FOR SAFARI BROWSER

--- app.js
+++ app.js
@@ -1,62 +1,58 @@
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const sanitizeV5 = require('./utilities/mongoSanitizeV5.js');
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const path = require("path")
-const ejsMate = require("ejs-mate")
-const methodOverride = require("method-override")
-const session = require("express-session")
+const sanitizeV5 = require("./utilities/mongoSanitizeV5.js");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const methodOverride = require("method-override");
+const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utilities/expressError");
-const passport = require("passport")
-const LocalStrategy = require("passport-local")
-const User = require("./models/users")
-const helmet = require("helmet")
-const questions = require("./questions")
-const MongoStore = require('connect-mongo');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/users");
+const helmet = require("helmet");
+const questions = require("./questions");
+const MongoStore = require("connect-mongo");
 
+const { storeReturnTo } = require("./utilities/middleware");
 
-const {storeReturnTo} = require("./utilities/middleware")
+const userRoute = require("./routes/journal");
+const loginRegisterRoute = require("./routes/loginRegister");
+const feedRoute = require("./routes/feed");
 
-const userRoute = require("./routes/journal")
-const loginRegisterRoute = require("./routes/loginRegister")
-const feedRoute = require("./routes/feed")
+const dbUrl =
+  process.env.DB_URL || "mongodb://127.0.0.1:27017/TodayIAskedMyself";
 
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/TodayIAskedMyself"
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Connected to TIAM DB"))
+  .catch((error) => console.log("Error connecting to Rimi DB", error));
 
+app.engine("ejs", ejsMate);
 
-mongoose 
-.connect(dbUrl)
-.then(() => console.log("Connected to TIAM DB"))
-.catch((error) => console.log("Error connecting to Rimi DB", error))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.engine("ejs", ejsMate)
-
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
-
-
-
-
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(sanitizeV5({ replaceWith: '_' }));
+app.use(sanitizeV5({ replaceWith: "_" }));
 
-const secret = process.env.SECRET || "mveMjsu9p"
+const secret = process.env.SECRET || "mveMjsu9p";
 
 const store = MongoStore.create({
-  mongoUrl:dbUrl,
+  mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
-  secret
-})
+  secret,
+});
 store.on("error", (error) => {
-  console.log("error connecting session to mongodb", error)
-})
+  console.log("error connecting session to mongodb", error);
+});
 
 const sessionConfig = {
   store,
@@ -72,70 +68,73 @@ const sessionConfig = {
   },
 };
 app.use(session(sessionConfig));
-app.use(flash())
+app.use(flash());
 
 //allow cross origin scripts
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
 ];
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net",
+  "https://kit-free.fontawesome.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
 ];
 const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
 ];
-const fontSrcUrls = [];
+const fontSrcUrls = [
+  "https://fonts.gstatic.com/",
+  "https://use.fontawesome.com/",
+];
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dzm6syibq/", 
-                "https://images.unsplash.com/",
-            ],
-            fontSrc: ["'self'", ...fontSrcUrls],
-        },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", "blob:"],
+      objectSrc: [],
+      imgSrc: [
+        "'self'",
+        "blob:",
+        "data:",
+        "https://res.cloudinary.com/dzm6syibq/",
+        "https://images.unsplash.com/",
+      ],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
 );
 
 //Passport for authentication and sessions
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use(storeReturnTo)
+app.use(storeReturnTo);
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
-  res.locals.success = req.flash("success")
-  res.locals.error = req.flash("error")
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   res.locals.questionsArray = questions;
-  next()  
-}); 
-
+  next();
+});
 
 //Middleware for return back to previous url before clicking login button (/login)
 // app.use((req, res, next) => {
@@ -145,23 +144,20 @@ app.use((req, res, next) => {
 //   next()
 // })
 
-const question = (q) => q[Math.floor(Math.random() * q.length)]
+const question = (q) => q[Math.floor(Math.random() * q.length)];
 
 //ROUTES
-app.use("/", loginRegisterRoute)
-app.use("/feed", feedRoute)
-app.use("/user", userRoute)
+app.use("/", loginRegisterRoute);
+app.use("/feed", feedRoute);
+app.use("/user", userRoute);
 app.get("/", (req, res) => {
-  const q = question(questions)
-  res.render("home", {question: q})
-})
-
-
+  const q = question(questions);
+  res.render("home", { question: q });
+});
 
 app.all(/(.*)/, (req, res, next) => {
   next(new ExpressError("Page not found", 404));
 });
-
 
 //ERROR HANDLING
 app.use((err, req, res, next) => {
@@ -171,5 +167,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000")
-})
+  console.log("LISTENING ON PORT 3000");
+});
